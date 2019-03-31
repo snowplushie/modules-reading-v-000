dad08259e96ffc99aff8834b1520b618a99b8e5b
Paige Wiener <snowplushie@yahoo.com>
2019-03-31 10:05:29 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -13,4 +13,15 @@ module FancyDance
       "I'm doing a pirouette"
     end
     
-    def take_
+    def take_a_bow
+      "Thank you, thank you. It was a pleasure to dance for you all."
+    end
+  end
+  
+  module ClassMethods
+    
+    def metadata
+      "This class produces objects that love to dance."
+    end
+  end
+end
