882eabee9ce0b584cf97dd22a2c60fc8317e4cdf
Paige Wiener <snowplushie@yahoo.com>
2019-03-31 10:03:09 +0000

    Automatically backed up by Learn

---
+++ lib/fancy_dance.rb
@@ -0,0 +1,16 @@
+module FancyDance
+  module InstanceMethods
+    
+    def twirl
+      "I'm twirling!"
+    end
+    
+    def jump
+      "Look how high I'm jumping!"
+    end
+    
+    def pirouette
+      "I'm doing a pirouette"
+    end
+    
+    def take_
